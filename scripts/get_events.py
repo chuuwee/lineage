918617b8e878c49ec4a6b9772e67a2ce9a2cf892
chuuwee <chuuwee.eq@gmail.com>
2023-04-20 00:25:43 -0700

    feat: support retrieving scheduled events

---
+++ scripts/get_events.py
@@ -0,0 +1,39 @@
+import requests
+import pickle
+from bs4 import BeautifulSoup
+
+def get_events():
+# Load the serialized cookies from a file
+  with open('cookies.pkl', 'rb') as f:
+    cookies = pickle.load(f)
+
+# Make a GET request to the site with the loaded cookies
+  response = requests.get('http://lineageeq.dkpsystem.com/adminevent.php', cookies=cookies)
+  soup = BeautifulSoup(response.text, 'html.parser')
+
+# Extract the name and identifier values
+  table_rows = soup.css.select('#lanCurrent tr')
+  data = []
+  for row in table_rows[3:]:
+    tds = row.css.select('td')
+    date_str = tds[0].text.strip()
+    category_name = tds[1].text.strip()
+    event_name = tds[2].text.strip()
+    identifier = int(row.css.select('a')[0].attrs['href'].split('=')[1])
+    data.append({
+      'date': date_str,
+      'category_name': category_name,
+      'name': event_name,
+      'id': identifier,
+    })
+
+  return data
+
+def print_event(event):
+  print('{: >2} - {} - {} - {}'.format((event['id']), event['date'], event['category_name'], event['name']))
+
+if __name__ == "__main__":
+  events = get_events()
+# Print the extracted data
+  for event in events:
+    print_event(event)

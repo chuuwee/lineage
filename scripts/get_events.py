72830b7c72c2e84c2f27b527125d5ca700cf4655
chuuwee <chuuwee.eq@gmail.com>
2023-04-21 08:41:43 -0700

    feat: support event retrieval post creation for dkp upload

--- scripts/get_events.py
+++ scripts/get_events.py
@@ -1,5 +1,6 @@
 import requests
 import pickle
+from datetime import datetime
 from bs4 import BeautifulSoup
 
 def get_events():
@@ -17,11 +18,12 @@ def get_events():
   for row in table_rows[3:]:
     tds = row.css.select('td')
     date_str = tds[0].text.strip()
+    date_obj = datetime.strptime(date_str, '%m/%d/%y %I:%M%p')
     category_name = tds[1].text.strip()
     event_name = tds[2].text.strip()
     identifier = int(row.css.select('a')[0].attrs['href'].split('=')[1])
     data.append({
-      'date': date_str,
+      'date': date_obj,
       'category_name': category_name,
       'name': event_name,
       'id': identifier,
@@ -30,10 +32,10 @@ def get_events():
   return data
 
 def print_event(event):
-  print('{: >2} - {} - {} - {}'.format((event['id']), event['date'], event['category_name'], event['name']))
+  date_str = datetime.strftime(event['date'], '%m/%d/%y %I:%M%p').lower()
+  print('{: >2} - {} - {} - {}'.format((event['id']), date_str, event['category_name'], event['name']))
 
 if __name__ == "__main__":
   events = get_events()
-# Print the extracted data
   for event in events:
     print_event(event)
